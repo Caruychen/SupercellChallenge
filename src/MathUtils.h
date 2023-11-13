5e7a4cbfd1d7cfc19d49ef2cb326456fa504cc1c
Caruy Chen <caruychen@gmail.com>
2023-11-13 14:59:43 +0200

    added clamp functions

--- src/MathUtils.h
+++ src/MathUtils.h
@@ -7,3 +7,15 @@ float VecLength(sf::Vector2f a)
 {
     return sqrtf(a.x*a.x + a.y*a.y);
 }
+
+void _clamp(float *value, const float max)
+{
+    if (abs(*value) > max)
+        *value = *value < 0.f ? -max : max;
+}
+
+void _clamp(sf::Vector2f *value, const sf::Vector2f max)
+{
+    _clamp(&value->x, max.x);
+    _clamp(&value->y, max.y);
+}
