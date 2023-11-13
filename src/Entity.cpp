bf2aa13d01b6af1758b8f98415085ca5bd269075
Caruy Chen <caruychen@gmail.com>
2023-11-13 14:50:29 +0200

    setup basic entity class

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -1,8 +1,63 @@
 #include "Entity.h"
 
-Entity::Entity(EntityHandler *handler) :
-    m_handler(handler)
+Entity::Entity(
+               EntityHandler *handler,
+               const sf::Vector2f size,
+               const sf::Vector2f maxVelocity,
+               const sf::Vector2f maxAcceleration) :
+m_handler(handler),
+m_size(size),
+m_maxVelocity(maxVelocity),
+m_maxAcceleration(maxAcceleration),
+m_spawnPosition({0.f, 0.f}),
+m_position({0.f, 0.f}),
+m_velocity({0.f, 0.f}),
+m_acceleration({0.f, 0.f}),
+m_isMoving(false),
+m_collidingX(false),
+m_collidingY(false),
+m_collidingYBottom(false),
+m_box(sf::FloatRect(m_position.x, m_position.y, size.x, size.y))
 {}
 
 Entity::~Entity()
+{}
+
+void Entity::move(const float x, const float y)
+{
+    move(sf::Vector2f(x, y));
+}
+
+void Entity::move(sf::Vector2f pos)
+{}
+
+void Entity::addVelocity(const float x, const float y)
+{
+    addVelocity(sf::Vector2f(x, y));
+}
+
+void Entity::addVelocity(const sf::Vector2f value)
+{}
+
+void Entity::accelerate(const float x, const float y)
+{
+    accelerate(sf::Vector2f(x, y));
+}
+
+void Entity::accelerate(const sf::Vector2f value)
+{}
+
+void Entity::addFriction(const float deltaSeconds)
+{}
+
+void Entity::update()
+{}
+
+void Entity::_updateBox()
+{}
+
+void Entity::_checkCollisions()
+{}
+
+void Entity::_resolveCollisions()
 {}
--- src/Entity.h
+++ src/Entity.h
@@ -1,15 +1,47 @@
 #pragma once
 
+#include <SFML/Graphics.hpp>
+
 class EntityHandler;
 
 class Entity
 {
 public:
-    Entity(EntityHandler *handler);
+    Entity(
+           EntityHandler *handler,
+           const sf::Vector2f size,
+           const sf::Vector2f maxVelocity,
+           const sf::Vector2f maxAcceleration);
     ~Entity();
     
+    void move(const float x, const float y);
+    void move(sf::Vector2f pos);
+    void addVelocity(const float x, const float y);
+    void addVelocity(const sf::Vector2f value);
+    void accelerate(const float x, const float y);
+    void accelerate(const sf::Vector2f value);
+    void addFriction(const float deltaSeconds);
+    
+    virtual void update();
+    
 protected:
-    EntityHandler *m_handler;
+    void _updateBox();
+    virtual void _checkCollisions();
+    virtual void _resolveCollisions();
+    
+    EntityHandler   *m_handler;
+    sf::Vector2f        m_velocity;
+    sf::Vector2f        m_acceleration;
     
 private:
+    const sf::Vector2f  m_size;
+    const sf::Vector2f  m_maxVelocity;
+    const sf::Vector2f  m_maxAcceleration;
+    sf::Vector2f        m_spawnPosition;
+    sf::Vector2f        m_position;
+    bool                m_isMoving;
+    bool                m_collidingX;
+    bool                m_collidingY;
+    bool                m_collidingYBottom;
+    sf::FloatRect       m_box;
 };
